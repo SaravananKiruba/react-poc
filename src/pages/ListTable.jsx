13d4a98b50d1a593c1d46396f2a7e09d4babc9b8
Saravanan kiruba S <kirubachetti@gmail.com>
2023-11-19 13:50:15 +0530

    table working- kiruba

--- src/pages/ListTable.jsx
+++ src/pages/ListTable.jsx
@@ -4,37 +4,25 @@ import axios from 'axios';
 const OrderTable = () => {
   const [orderDetails, setOrderDetails] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [sortColumn, setSortColumn] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [filters, setFilters] = useState({
+    orderNumber: '',
+    orderDate: '',
+    entryUser: '',
+    cse: '',
+    owner: '',
+    clientName: '',
+  });
 
   useEffect(() => {
-    //GetOrderdeatilsbyorderNo(7485);
-    getlatest100Records();
-    //GetOrderDetails();
+    getLatest100Records();
   }, []);
 
-  const getlatest100Records = async () => {
+  const getLatest100Records = async () => {
     try {
       const response = await axios.get('http://127.0.0.1:8000/api/getlatest100Records');
-      setOrderDetails(response.data.data);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching order details:', error);
-      setLoading(false);
-    }
-  };
-  const GetOrderDetails = async () => {
-    try {
-      const response = await axios.get('http://127.0.0.1:8000/api/getorderdetails');
-      setOrderDetails(response.data.data);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching order details:', error);
-      setLoading(false);
-    }
-  };
-  const GetOrderdeatilsbyorderNo = async (orderNumber) => {
-    try {
-      const response = await axios.get(`http://127.0.0.1:8000/api/order-details/${orderNumber}`);
-      setOrderDetails(response.data.data);
+      setOrderDetails(response.data);
       setLoading(false);
     } catch (error) {
       console.error('Error fetching order details:', error);
@@ -42,47 +30,139 @@ const OrderTable = () => {
     }
   };
 
+  const handleSort = (column) => {
+    setSortColumn(column);
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  };
+  const FilterInput = ({ value, onChange, placeholder }) => {
+    return (
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        className="px-2 py-1 border rounded-md"
+      />
+    );
+  };
+  
+  // ...
+  
+  // Inside OrderTable component
+  const FilterRow = () => {
+    return (
+      <div className="flex mb-4">
+        {/* ... Other filters */}
+        <FilterInput
+          value={filters.orderNumber}
+          onChange={(value) => handleFilterChange('orderNumber', value)}
+          placeholder="Filter Order Number"
+        />
+        {/* ... Other filters */}
+      </div>
+    );
+  };
+  const TableHeader = ({ column, children }) => (
+    <th
+      className="py-2 px-4 border-b font-bold bg-gray-200 cursor-pointer"
+      onClick={() => handleSort(column)}
+    >
+      {children} {sortColumn === column && (sortOrder === 'asc' ? '▲' : '▼')}
+    </th>
+  );
+  
+  // ...
+  
+  // Inside the table
+  <thead>
+    <tr>
+      <TableHeader column="orderNumber">Order Number</TableHeader>
+      <TableHeader column="orderDate">Order Date</TableHeader>
+      {/* ... Other columns */}
+    </tr>
+  </thead>
+  
+  
+
+  const handleFilterChange = (column, value) => {
+    setFilters({ ...filters, [column]: value });
+  };
+
+  const filteredAndSortedOrders = () => {
+    let filteredOrders = orderDetails?.data ? [...orderDetails.data] : [];
+
+    Object.keys(filters).forEach((key) => {
+      const filterValue = filters[key].toLowerCase();
+      if (filterValue) {
+        filteredOrders = filteredOrders.filter((order) => order[key].toLowerCase().includes(filterValue));
+      }
+    });
+
+    if (sortColumn) {
+      filteredOrders.sort((a, b) => {
+        const aValue = a[sortColumn].toLowerCase();
+        const bValue = b[sortColumn].toLowerCase();
+
+        if (sortOrder === 'asc') {
+          return aValue.localeCompare(bValue);
+        } else {
+          return bValue.localeCompare(aValue);
+        }
+      });
+    }
+
+    return filteredOrders;
+  };
+
+  const displayedOrders = filteredAndSortedOrders();
 
   return (
     <div className="p-4">
       {loading ? (
         <p>Loading...</p>
-      ) : orderDetails && orderDetails.length > 0 ? (
+      ) : displayedOrders && displayedOrders.length > 0 ? (
         <table className="min-w-full bg-white border border-gray-300">
           <thead>
             <tr>
-              <th class="py-2 px-4 border-b font-bold bg-gray-200">Order Number</th>
-              <th class="py-2 px-4 border-b font-bold bg-gray-200">Order Date</th>
-              <th class="py-2 px-4 border-b font-bold bg-gray-200">Entry User</th>
-              <th class="py-2 px-4 border-b font-bold bg-gray-200">CSE</th>
-              <th class="py-2 px-4 border-b font-bold bg-gray-200">Owner</th>
-              <th class="py-2 px-4 border-b font-bold bg-gray-200">Client Name</th>
+              <th className="py-2 px-4 border-b font-bold bg-gray-200">Order Number</th>
+              <th className="py-2 px-4 border-b font-bold bg-gray-200">Order Date</th>
+              <th className="py-2 px-4 border-b font-bold bg-gray-200">Entry User</th>
+              <th className="py-2 px-4 border-b font-bold bg-gray-200">CSE</th>
+              <th className="py-2 px-4 border-b font-bold bg-gray-200">Owner</th>
+              <th className="py-2 px-4 border-b font-bold bg-gray-200">Client Name</th>
             </tr>
           </thead>
-
           <tbody>
-            {orderDetails.map((order) => (
+            {displayedOrders.map((order) => (
               <tr key={order.OrderNumber}>
                 <td className="py-2 px-4 border-b text-center">{order.OrderNumber}</td>
                 <td className="py-2 px-4 border-b text-center">{order.OrderDate}</td>
                 <td className="py-2 px-4 border-b text-center">{order.EntryUser}</td>
                 <td className="py-2 px-4 border-b text-center">{order.CSE}</td>
                 <td className="py-2 px-4 border-b text-center">{order.Owner}</td>
-                <td className="py-2 px-4 border-b text-center">{order.ClientName}</td>
-              </tr>
+                <td className="py-2 px-4 border-b text-center">{order.ClientName}</td>              </tr>
             ))}
           </tbody>
-
         </table>
       ) : (
-        <p>No order details available</p>
+        <div>
+          <p>No order details available</p>
+          {/* Render table headers even when no data is available */}
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b font-bold bg-gray-200">Order Number</th>
+                <th className="py-2 px-4 border-b font-bold bg-gray-200">Order Date</th>
+                <th className="py-2 px-4 border-b font-bold bg-gray-200">Entry User</th>
+                <th className="py-2 px-4 border-b font-bold bg-gray-200">CSE</th>
+                <th className="py-2 px-4 border-b font-bold bg-gray-200">Owner</th>
+                <th className="py-2 px-4 border-b font-bold bg-gray-200">Client Name</th>
+              </tr>
+            </thead>          </table>
+        </div>
       )}
-
     </div>
   );
-
-
 };
 
 export default OrderTable;
-
