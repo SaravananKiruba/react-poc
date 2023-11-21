1583b0260d4d551c1041323b18f633e299479ee8
Saravanan Kiruba S <saravanan.s@altrockstech.com>
2023-11-21 10:48:46 +0530

    sort issue fixed -- Sirnjeevi

--- src/components/OrderTableHeader.jsx
+++ src/components/OrderTableHeader.jsx
@@ -1,10 +1,9 @@
 const OrderTableHeader = ({ column, children, sortOrder, onSort }) => (
-    <th
-        className="py-2 px-4 border-b font-bold bg-gray-200 cursor-pointer"
-        onClick={() => onSort(column)}
-    >
+    <th className="py-2 px-4 border-b font-bold bg-gray-200 cursor-pointer">
+      <button onClick={() => onSort(column)} className="focus:outline-none">
         {children} {sortOrder === 'asc' ? '▲' : '▼'}
+      </button>
     </th>
-);
-
-export default OrderTableHeader;
+  );
+  
+  export default OrderTableHeader;
--- src/pages/OrderTable.jsx
+++ src/pages/OrderTable.jsx
@@ -1,144 +1,146 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-// Import additional components
-import OrderTableHeader from '../components/OrderTableHeader'; // Assuming this component is in a separate file
-import OrderTableRow from '../components/OrderTableRow'; // Assuming this component is in a separate file
+import OrderTableHeader from '../components/OrderTableHeader';
+import OrderTableRow from '../components/OrderTableRow';
 
 const OrderTable = () => {
-  const [orderDetails, setOrderDetails] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [sortColumn, setSortColumn] = useState(null);
-  const [sortOrder, setSortOrder] = useState('asc');
-  const [filters, setFilters] = useState({
-    orderNumber: '',
-    orderDate: '',
-    entryUser: '',
-    cse: '',
-    owner: '',
-    clientName: '',
-  });
+    const [orderDetails, setOrderDetails] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [sortColumn, setSortColumn] = useState();
+    const [sortOrder, setSortOrder] = useState('asc');
+    const [filters, setFilters] = useState({
+        orderNumber: '',
+        orderDate: '',
+        entryUser: '',
+        cse: '',
+        owner: '',
+        clientName: '',
+    });
+    const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    getLatest100Records();
-  }, []);
+    const getOrders = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/getOrders', {
+                params: {
+                    page: currentPage,
+                    perPage: 25,
+                    sortColumn,
+                    sortOrder,
+                    ...filters,
+                },
+            });
+            if (response.data && response.data.success) {
+                setOrderDetails(response.data.data.data);
+            } else {
+                console.error('Invalid API response:', response.data);
+                alert('Invalid API response');
+            }
 
-  const getLatest100Records = async () => {
-    try {
-      const response = await axios.get('http://127.0.0.1:8000/api/getlatest100Records');
-      
-      if (response.data && Array.isArray(response.data.data)) {
-        // Update state only if the response is valid
-        setOrderDetails(response.data);
-      } else {
-        console.error('Invalid API response:', response.data);
-      }
-  
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching order details:', error);
-      setLoading(false);
-    }
-  };
-  
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching order details:', error);
+            setLoading(false);
+        }
+    };
 
-  const handleSort = (column) => {
-    setSortColumn(column);
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
-  };
+    useEffect(() => {
+       getOrders();
+        const fetchData = async () => {
+            try {
+                await getOrders();
+                filteredAndSortedOrders();
+            } catch (error) {
+                console.error('Error fetching orders:', error);
+            }
+        };
 
-  const handleFilterChange = (column, value) => {
-    setFilters({ ...filters, [column]: value });
-  };
+        fetchData();
+    }, [currentPage, sortColumn, sortOrder, filters]);
 
-  const filteredAndSortedOrders = () => {
-    let filteredOrders = orderDetails?.data ? [...orderDetails.data] : [];
+    const handleSort = (column) => {
+        setSortColumn(column);
+        setSortOrder((prevSortOrder) => (prevSortOrder === 'asc' ? 'desc' : 'asc'));
+    };
 
-    filteredOrders = filteredOrders.filter((order) =>
-      Object.entries(filters).every(([key, value]) =>
-        order[key].toLowerCase().includes(value.toLowerCase())
-      )
-    );
+    const handleFilterChange = (column, value) => {
+        setFilters({ ...filters, [column]: value });
+    };
 
-    if (sortColumn) {
-      filteredOrders.sort((a, b) => {
-        const aValue = a[sortColumn].toLowerCase();
-        const bValue = b[sortColumn].toLowerCase();
-        return sortOrder === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
-      });
-    }
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
 
-    return filteredOrders;
-  };
+    const filteredAndSortedOrders = () => {
+        let filteredOrders = orderDetails != null ? [orderDetails] : [];
 
-  const displayedOrders = filteredAndSortedOrders();
+        // filteredOrders = filteredOrders.filter((order) =>
+        //     Object.entries(filters).every(([key, value]) =>
+        //         order[key] && order[key].toLowerCase().includes(value.toLowerCase())
+        //     )
+        // );
+        // if (sortColumn) {
+        //     filteredOrders.sort((a, b) => {
+        //         const aValue = a[sortColumn] ? a[sortColumn].toLowerCase() : '';
+        //         const bValue = b[sortColumn] ? b[sortColumn].toLowerCase() : '';
+        //         return sortOrder === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+        //     });
+        // }
+        return filteredOrders[0];
+    };
+    const displayedOrders = filteredAndSortedOrders();
 
-  return (
-    <div className="p-4">
-      {loading ? (
-        <p>Loading...</p>
-      ) : displayedOrders && displayedOrders.length > 0 ? (
-        <table className="min-w-full bg-white border border-gray-300">
-          <thead>
-            <tr>
-              <OrderTableHeader column="orderNumber" sortOrder={sortOrder} onSort={handleSort}>
-                Order Number
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                Order Date
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                Entry User
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                CSE
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                Owner
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                Client Name
-              </OrderTableHeader>
-            </tr>
-          </thead>
-          <tbody>
-            {displayedOrders.map((order) => (
-              <OrderTableRow key={order.OrderNumber} order={order} />
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <div>
-          <p>No order details available</p>
-          {/* Render table headers even when no data is available */}
-          <table className="min-w-full bg-white border border-gray-300">
-            <thead>
-              <tr>
-                <OrderTableHeader column="orderNumber" sortOrder={sortOrder} onSort={handleSort}>
-                  Order Number
-                </OrderTableHeader>
-                <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                  Order Date
-                </OrderTableHeader>
-                <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                Entry User
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                CSE
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                Owner
-              </OrderTableHeader>
-              <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
-                Client Name
-              </OrderTableHeader>
-              </tr>
-            </thead>
-          </table>
+    return (
+
+        <div className="p-4">
+            {loading ? (
+                <p>Loading...</p>
+            ) : displayedOrders && displayedOrders.length > 0 ? (
+                <div>
+                    <table className="min-w-full bg-white border border-gray-300">
+                        <OrderTableHeader column="orderNumber" sortOrder={sortOrder} onSort={handleSort}>
+                            Order Number
+                        </OrderTableHeader>
+                        <OrderTableHeader column="orderDate" sortOrder={sortOrder} onSort={handleSort}>
+                            Order Date
+                        </OrderTableHeader>
+                        <OrderTableHeader column="entryUser" sortOrder={sortOrder} onSort={handleSort}>
+                            Entry User
+                        </OrderTableHeader>
+                        <OrderTableHeader column="cse" sortOrder={sortOrder} onSort={handleSort}>
+                            CSE
+                        </OrderTableHeader>
+                        <OrderTableHeader column="owner" sortOrder={sortOrder} onSort={handleSort}>
+                            Owner
+                        </OrderTableHeader>
+                        <OrderTableHeader column="clientName" sortOrder={sortOrder} onSort={handleSort}>
+                            Client Name
+                        </OrderTableHeader>
+                        <tbody>
+                            {displayedOrders.map((order) => (
+                                <OrderTableRow key={order.OrderNumber} order={order} />
+                            ))}
+                        </tbody>
+                    </table>
+                    <div className="pagination">
+                        <button
+                            onClick={() => handlePageChange(currentPage - 1)}
+                            disabled={currentPage === 1}
+                        >
+                            Previous
+                        </button>
+                        <span>Page {currentPage}</span>
+                        <button onClick={() => handlePageChange(currentPage + 1)}>
+                            Next
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <p>No order details available</p>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default OrderTable;
