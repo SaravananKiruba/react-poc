a238778523c371e45eccb772c0fc1ea7b17a78a0
Saravanan Kiruba S <saravanan.s@altrockstech.com>
2023-11-23 18:13:22 +0530

    final home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Chart from 'chart.js/auto';
 import axios from 'axios';
+import Chart from 'chart.js/auto';
 
 const Home = () => {
   const [workorders, setWorkorders] = useState([]);
@@ -14,14 +14,23 @@ const Home = () => {
       const response = await axios.get('http://127.0.0.1:8000/api/getorderdetails');
       const workordersData = response.data.success ? response.data.data : [];
       setWorkorders(workordersData);
-      createPieAndBarCharts(workordersData);
+      createCharts(workordersData);
     } catch (error) {
       console.error('Error fetching data:', error.message);
     }
   };
 
-  const createChart = (chartType, chartData) => {
-    const chartElement = document.getElementById(chartType + 'Chart');
+  const createCharts = (workordersData) => {
+    const cancelledOrdersCount = workordersData.filter(order => order.CancelFlag === 1).length;
+    const activeOrdersCount = workordersData.filter(order => order.CancelFlag === 0).length;
+
+    createChart('pie', 'pieChart', [cancelledOrdersCount, activeOrdersCount]);
+    createChart('bar', 'barChart', [cancelledOrdersCount, activeOrdersCount]);
+    createChart('doughnut', 'doughnutChart', [cancelledOrdersCount, activeOrdersCount]);
+  };
+
+  const createChart = (chartType, chartId, chartData) => {
+    const chartElement = document.getElementById(chartId);
     const context = chartElement.getContext('2d');
 
     Chart.getChart(context)?.destroy();
@@ -40,16 +49,9 @@ const Home = () => {
     });
   };
 
-  const createPieAndBarCharts = (workordersData) => {
-    const cancelledOrdersCount = workordersData.filter(order => order.CancelFlag === 1).length;
-    const activeOrdersCount = workordersData.filter(order => order.CancelFlag === 0).length;
-
-    createChart('pie', [cancelledOrdersCount, activeOrdersCount]);
-    createChart('bar', [cancelledOrdersCount, activeOrdersCount]);
-    createChart('doughnut', [cancelledOrdersCount, activeOrdersCount]);
-    createChart('polarArea', [cancelledOrdersCount, activeOrdersCount]);
-  };
-
+  const totalWorkorders = workorders.length;
+  const activeWorkorders = workorders.filter(order => order.CancelFlag === 0).length;
+  const cancelledWorkorders = workorders.filter(order => order.CancelFlag === 1).length;
 
   return (
     <div className="container mx-auto p-4">
@@ -59,33 +61,34 @@ const Home = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="p-4 bg-blue-500 text-white rounded shadow-md">
           <h2 className="text-xl font-bold mb-2">Total Workorders</h2>
-          <p className="text-3xl">{workorders.length}</p>
+          <p className="text-3xl">{totalWorkorders}</p>
         </div>
         <div className="p-4 bg-blue-500 text-white rounded shadow-md">
           <h2 className="text-xl font-bold mb-2">Active Workorders</h2>
-          <p className="text-3xl">{workorders.filter(order => order.CancelFlag === 0).length}</p>
+          <p className="text-3xl">{activeWorkorders}</p>
         </div>
         <div className="p-4 bg-blue-500 text-white rounded shadow-md">
           <h2 className="text-xl font-bold mb-2">Cancelled Workorders</h2>
-          <p className="text-3xl">{workorders.filter(order => order.CancelFlag === 1).length}</p>
+          <p className="text-3xl">{cancelledWorkorders}</p>
         </div>
       </div>
-      <div className="mt-8">
+      <div className="mt-4">
         <div className="flex">
-          <div className="w-1/2">
+          <div className="w-1/2 p-4">
             <h2 className="text-center mb-4">Pie Chart</h2>
             <canvas id="pieChart" className="w-full h-40 mx-auto"></canvas>
-            <h2 className="text-center mb-4 mt-4">Doughnut Chart</h2>
-            <canvas id="doughnutChart" className="w-full h-40 mx-auto"></canvas>            
           </div>
-          <div className='w-1/2'>
-            <h2 className="text-center mb-4">Polar Area Chart</h2>
-            <canvas id="polarAreaChart" className="w-full h-40 mx-auto"></canvas>
-            <h2 className="text-center mb-4 mt-4">Bar Chart</h2>
-            <canvas id="barChart" className="w-full h-40 mx-auto"></canvas>            
+          <div className='w-1/2 p-4'>
+            <h2 className="text-center mb-4 mt-4">Doughnut Chart</h2>
+            <canvas id="doughnutChart" className="w-full h-40 mx-auto"></canvas>
           </div>
         </div>
+        <div className='w-full'>
+          <h2 className="text-center mb-4 mt-4">Bar Chart</h2>
+          <canvas id="barChart" className="w-full h-40 mx-auto"></canvas>
+        </div>
       </div>
+
     </div>
   );
 };
