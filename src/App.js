7419709d45cf405fe7f2facf3fb320013ec90a7e
Saravanan Kiruba S <saravanan.s@altrockstech.com>
2023-11-24 17:47:07 +0530

    code clean -- kiruba

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import OrderTable from './pages/OrderTable';
-import LoginForm from './pages/LoginForm'; // Import the LoginForm component
+import LoginForm from './pages/LoginForm';
 import './index.css';
 import './App.css';
 
@@ -13,8 +13,8 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/home" element={<Home />} />
+          {/* <Route path="/" element={<LoginForm />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/OrderTable" element={<OrderTable />} />
         </Routes>
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="bg-blue-500 p-4">
       <div className="flex flex-col md:flex-row md:justify-start">
-        <Link to="/home" className="text-white mb-2 md:mb-0 md:mr-4">
+        <Link to="/" className="text-white mb-2 md:mb-0 md:mr-4">
           Home
         </Link>
         <Link to="/OrderTable" className="text-white mb-2 md:mb-0 md:mr-4">
